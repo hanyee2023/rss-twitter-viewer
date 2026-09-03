a0033e24e21c151bfc3e0a3a16dee9d52dc411ac
hanyee2023 <142211294+hanyee2023@users.noreply.github.com>
2026-09-03 15:09:49 +0800

    Update core.js

--- js/core.js
+++ js/core.js
@@ -38,7 +38,10 @@ const FORCE_PROXY_HOSTS = [
     "htumeng.com",
     "642p.com",
     "tutu1.space",
-    "freeshare58.com"
+    "freeshare58.com",
+    "jhfduadgiyhas.com",
+    "000355.xyz",
+    "919129.xyz"
 ];
 
 function normalizeUrl(rawUrl) {
