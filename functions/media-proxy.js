6701936317281524aca417ccf8cfab5d73bf2d4a
hanyee2023 <142211294+hanyee2023@users.noreply.github.com>
2026-09-03 15:09:21 +0800

    Add new domains to media proxy whitelist

--- functions/media-proxy.js
+++ functions/media-proxy.js
@@ -23,7 +23,10 @@ const ALLOW_PROXY_HOSTS = [
   "htumeng.com",
   "642p.com",
   "tutu1.space",
-  "freeshare58.com"
+  "freeshare58.com",
+  "jhfduadgiyhas.com",
+  "000355.xyz",
+  "919129.xyz"
 ];
 
 function normalizeHost(host) {
